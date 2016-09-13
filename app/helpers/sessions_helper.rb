c870eeaa932313c6e22e69c4bc3fcf6ab92ae806
Ahmed Elsayed <ahmed.elsayed.mahmoud3@gmail.com>
2016-09-13 17:52:32 +0200

    version 1

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,54 @@
+module SessionsHelper
+	 def log_in(organization)
+		session[:organization_id] = organization.id
+		 #Just a cookie produced in the browser and stays till u sign out or close the browser
+	end
+
+	def current_organization
+		@current_organization = @current_organization || Organization.find_by(id: session[:organization_id])
+	end
+
+	def logged_in?
+		!current_organization.nil?
+	end
+
+	def log_out
+
+		session.delete(:organization_id)
+		@current_organization = nil
+	end
+
+	def donor_log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		@current_user = @current_user || User.find_by(id: session[:user_id])
+	end
+
+	def donor_logged_in?
+		!current_user.nil?
+	end
+	def donor_log_out
+
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
+	def admin_log_in
+		session[:admin] = "admin"
+		@current_admin = "admin"
+	end
+
+	def current_admin
+		@current_admin = @current_user || session[:admin]
+	end
+
+	def admin_logged_in?
+		!current_admin.nil?
+	end
+	def admin_log_out
+		session.delete(:admin)
+		@current_admin = nil
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,45 @@
+Rails.application.routes.draw do
+
+  get 'admin/home'
+
+resources :organizations do
+  collection do
+    get    'notApproved' => 'organizations#notApproved' , as: 'notApproved'
+    get    'showNotApproved/:id' => 'organizations#showNotApproved' , as: 'showNotApproved'
+    post    'approveOrg/:id' => 'organizations#approveOrg' , as: 'approveOrg'
+    delete  'disapproveOrg/:id' => 'organizations#disapproveOrg' , as: 'disapproveOrg'
+  end
+end
+
+  resources :messages
+  resources :organizations do
+    resources :needs
+    get 'Achievements' => 'needs#indexAchivements'
+  end
+  resources :users
+  resources :organization_searches
+  root 'organizations#index'
+  get 'needs', to:'needs#index'
+  get 'achiev' , to:'needs#indexAchivements'
+  get    '/signUp',   to: 'organizations#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get '/organization/:id/editImages', to: 'organizations#editAndaddImages', as: 'editImages'
+  get    '/donor/register',   to: 'users#new'
+  get    '/donor/login',   to: 'sessions#new_donor'
+  post   '/donor/login',   to: 'sessions#create_donor'
+  delete '/donor/logout',  to: 'sessions#destroy_donor'
+  get    '/admin/login',   to: 'sessions#new_admin'
+  get    '/admin/',   to: 'sessions#new_admin'
+  post   '/admin/login',   to: 'sessions#create_admin'
+  delete '/admin/logout',  to: 'sessions#destroy_admin'
+  
+  post   '/messages/:id/',   to: 'messages#adminReply'
+  get 'auth/:provider/callback' => 'sessions#create_donor_provider'
+  get 'auth/failure' => redirect('/')
+
+
+#  root '/users'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
